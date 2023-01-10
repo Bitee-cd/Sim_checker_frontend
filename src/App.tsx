15cefa9a2bfb0b512cf475e4337c8f33432322e6
Bitee-cd <Calebduniya45@gmail.com>
2023-01-10 13:06:51 +0100

    min height and vertical alignment

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Steps from "./Components/Steps";
 function App() {
   return (
     <div className="">
-      <div className="App text-dark_purple bg-light_blue ">
+      <div className="App text-dark_purple bg-light_blue min-h-[100vh] flex items-center">
         <div className=" max-w-[1440px] w-[90%] md:w-[80%] mx-auto py-7 ">
           <Navbar />
           <SearchForm />
